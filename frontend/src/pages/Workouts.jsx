5d02d039e69d54ba930475589a4eb3592a8a6e75
Pavan Kumar <pavan.kumar310399@gmail.com>
2026-04-13 15:11:39 +0100

    chore: remove unnecessary React imports (post-merge cleanup)

--- frontend/src/pages/Workouts.jsx
+++ frontend/src/pages/Workouts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import { useState, useMemo } from "react";
 import { useData } from "../context/useData";
 import DateRangeFilter from "../components/DateRangeFilter";
 
